8417b99bb0cb307d25d30face74f9274d91fea3c
musk1n <08052006muskan@gmail.com>
2024-06-28 12:55:03 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
